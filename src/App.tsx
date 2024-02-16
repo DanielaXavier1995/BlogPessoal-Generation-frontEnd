5bd0d34ecfba77b68fa89d32062fc560dff6c5ad
Daniela Xavier <danielamxsoares@gmail.com>
2024-02-16 17:08:24 -0300

    refactor: ajustes para melhor funcionamento da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
+import Home from './paginas/home/Home';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './paginas/login/Login';
 
 function App() {
-
   return (
     <>
-      
+    <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
     </>
-  )
+);
 }
-
-export default App
+export default App;
